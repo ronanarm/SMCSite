a70bac010b3f11f1f74c20219ceea36dde0542b0
ronanarm <60759480+ronanarm@users.noreply.github.com>
2023-10-26 20:09:16 -0400

    Updated patchbay practice module with images and more details

--- gearLibrary.js
+++ gearLibrary.js
@@ -1,5 +1,91 @@
-const preamps = [
-    "API 1608", "BAE 1023 (Neve)", "Grace M501", "Millennia HVD8", "Purple Biz", "Chandler TG2-500"
-]
-const compressors = ["LA2A", "1176LN", "API 527", "BAE 10DCF", "SSL Bus Compressor", "API 2500"]
-const equalizers = ["API 550B", "BAE 1023"]
+const preamps = {
+    "API 1608": {
+        "in": "API Mic Pre Input [#]",
+        "out": "API Mic Pre Output",
+        "img": "img/outboard/api1608.webp",
+    },
+    "Neve 1023 (BAE)": {
+        "in": "Neve Mic Pre Input [#]",
+        "out": "Neve Line Output",
+        "img": "../img/outboard/bae1023.webp",
+    },
+    "Grace M501": {
+        "in": "Sweet 10 Rack (Bottom Row, Space 1 or 2)",
+        "out": "Sweet 10 Rack (Top Row, Space 1 or 2)",
+        "img": "../img/outboard/gracem501.webp",
+    },
+    "Chandler TG2": {
+        "in": "Sweet 10 Rack (Bottom Row, Space 3)",
+        "out": "Sweet 10 Rack (Top Row, Space 3)",
+        "img": "../img/outboard/chandlertg2.webp",
+    },
+    "Purple Biz": {
+        "in": "Sweet 10 Rack (Bottom Row, Space 4)",
+        "out": "Sweet 10 Rack (Top Row, Space 4)",
+        "img": "../img/outboard/purplebiz.webp",
+    },
+    "Millennia HV-3D/8": {
+        "in": "Millennia Pre Input",
+        "out": "Millennia Line Output",
+        "img": "../img/outboard/HV3d.png",
+    },
+}
+   //, , "Grace M501", "Millennia HVD8", "Purple Biz", "Chandler TG2-500"
+
+
+
+const compressors = {
+    "API 2500": {
+        "in": "API 2500 Bottom Row (L or R)",
+        "out": "API 2500 Top Row (L or R)",
+        "details": {
+            "type": "VCA",
+        },
+        
+        "img": "../img/outboard/api2500.webp",
+    },
+    "LA2A": {
+        "in": "LA2A Bottom Row (1 or 2)",
+        "out": "LA2A Top Row (1 or 2)",
+        "details": {
+            "type": "Optical",
+        },
+        "img": "../img/outboard/LA2A.webp",
+    },
+    "API 527": {
+        "in": "Echo Slot In (1-8)",
+        "out": "Echo Slot Out (1-8)",
+        "details": {
+            "type": "VCA",
+        },
+        "img": "../img/outboard/api527.webp",
+    },
+    "BAE 10DCF": {
+        "in": "BAE Bottom Row (1 or 2)",
+        "out": "BAE Top Row (1 or 2)",
+        "details": {
+            "type": "Diode Bridge",
+        },
+        "img": "../img/outboard/bae10dcf.webp",
+    },
+    "SSL GCOMP": {
+        "in": "GCOMP Bottom Row (L or R)",
+        "out": "GCOMP Top Row (L or R)",
+        "details": {
+            "type": "VCA",
+        },
+        "img": "../img/outboard/sslgcomp.webp",
+    },
+}
+const equalizers = {
+    "API 550B": {
+        "in": "API EQ Input",
+        "out": "API Insert Send OR API Direct Output",
+        "img": "../img/outboard/api550b.webp",
+    },
+    "Neve 1023 (BAE)": {
+        "in": "Neve Line Input",
+        "out": "Neve Line Output",
+        "img": "../img/outboard/bae1023.webp",
+    },
+}
--- gearRandomizer.js
+++ gearRandomizer.js
@@ -1,16 +1,52 @@
+let randomHTML = "";
+
+let pushToRandomObj = (object, type, inputQuery) => {
+    if(document.querySelector(`input[value="${inputQuery}"]`).checked){
+        let selectedOption = randomProp(object)
+        let objectDetails = object[selectedOption];
+        let additionalHTML = "";
+        let detailsHTML = "";
+        let imgHTML = "";
+        let addClass = "";
+        let patchbayDetails = "";
+        for (const property in objectDetails.details) {
+            detailsHTML += `<div class="detail"><span>${property}</span><span>${objectDetails.details[property]}</span></div>`;
+        }
+        if(inputQuery == "Mic"){
+            additionalHTML = objectDetails.manufacturer + " ";
+            addClass = "unclickable"
+        }
+        if(inputQuery != "Mic"){
+            imgHTML = `<img src="${objectDetails.img}" />`
+            patchbayDetails = `
+            <br>
+            <h4>Patchbay Details</h4>
+            <div class="detail"><span>Input</span><span>${objectDetails.in}</span></div>
+            <div class="detail"><span>Output</span><span>${objectDetails.out}</span></div>`
+        }
+        let objHTML = `
+        <div>
+            <h4>${type}</h4>
+            <span><button class="showDetails ${addClass}" onclick="showDetails(this)">${additionalHTML}${selectedOption}</button></span>
+            <button class="blackBack" onclick="showDetails(this)"></button>
+            <div class="outboardDetails">
+                ${imgHTML}
+                <div class="detailBlob">
+                    <h3>${additionalHTML}${selectedOption}</h3>
+                    ${detailsHTML}
+                    ${patchbayDetails}
+                </div>
+            </div>
+        </div>`
+        randomHTML = randomHTML + objHTML;
+    }
+}
+
 let generateRandom = () => {
     console.log("Generating")
-    let htmlToPush ="";
-    if(document.querySelector('input[value="Mic"]').checked){
-        let selectedMic = randomProp(mics)
-        let micDetails = mics[selectedMic];
-        let micHTML = `
-        <div>
-            <h4>Microphone</h4>
-            <span>${micDetails.manufacturer} ${selectedMic}</span>
-        </div>`
-        htmlToPush = htmlToPush + micHTML;
-    }
+    randomHTML = "";
+    pushToRandomObj(mics, "Microphone", "Mic")
+    
     if(document.querySelector('input[value="Input"]').checked){
         let inputValue = getRandomInt(60);
         let htmlPush = `
@@ -18,35 +54,23 @@ let generateRandom = () => {
             <h4>Mic Line</h4>
             <span>Input ${inputValue}</span>
         </div>`
-        htmlToPush = htmlToPush + htmlPush;
+        randomHTML += htmlPush;
     }
-    if(document.querySelector('input[value="Pre"]').checked){
-        const randomElement = preamps[Math.floor(Math.random() * preamps.length)];
-        let micHTML = `
-        <div>
-            <h4>Mic Preamp</h4>
-            <span>${randomElement}</span>
-        </div>`
-        htmlToPush = htmlToPush + micHTML;
+    pushToRandomObj(preamps, "Preamp", "Pre")
+    if(document.querySelector('input[value="EQFirst"]').checked){
+        pushToRandomObj(equalizers, "Equalizer", "EQ")
+        pushToRandomObj(compressors, "Compressor", "Comp")
+    }else if(document.querySelector('input[value="CompFirst"]').checked){
+        pushToRandomObj(compressors, "Compressor", "Comp")
+        pushToRandomObj(equalizers, "Equalizer", "EQ")
+    }else if(Math.random() > 0.5){
+        pushToRandomObj(compressors, "Compressor", "Comp")
+        pushToRandomObj(equalizers, "Equalizer", "EQ")
+    }else{
+        pushToRandomObj(equalizers, "Equalizer", "EQ")
+        pushToRandomObj(compressors, "Compressor", "Comp")
     }
-    if(document.querySelector('input[value="EQ"]').checked){
-        const randomElement = equalizers[Math.floor(Math.random() * equalizers.length)];
-        let micHTML = `
-        <div>
-            <h4>Equalizer</h4>
-            <span>${randomElement}</span>
-        </div>`
-        htmlToPush = htmlToPush + micHTML;
-    }
-    if(document.querySelector('input[value="Comp"]').checked){
-        const randomElement = compressors[Math.floor(Math.random() * compressors.length)];
-        let micHTML = `
-        <div>
-            <h4>Compressor</h4>
-            <span>${randomElement}</span>
-        </div>`
-        htmlToPush = htmlToPush + micHTML;
-    }
+    
     if(document.querySelector('input[value="DAWIn"]').checked){
         let inputValue = getRandomInt(32);
         let htmlPush = `
@@ -54,12 +78,16 @@ let generateRandom = () => {
             <h4>DAW Input</h4>
             <span>Input ${inputValue}</span>
         </div>`
-        htmlToPush = htmlToPush + htmlPush;
+        randomHTML += htmlPush;
     }
-    htmlToPush = `<div class="randomAnswer">${htmlToPush}</div>`
-    document.getElementById("gearList").innerHTML = htmlToPush
+    randomHTML = `<div class="randomAnswer">${randomHTML}</div>`
+    document.getElementById("gearList").innerHTML = randomHTML
 }
 const randomProp = obj => Object.keys(obj)[(Math.random() * Object.keys(obj).length) | 0];
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
+  }
+
+  let showDetails = (thi) => {
+    thi.closest("div").classList.toggle("activeDetails")
   }
