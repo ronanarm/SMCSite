f87c97e69173a654a5b0692c28a0d82ac3ae4c8f
ronanarm <60759480+ronanarm@users.noreply.github.com>
2023-11-08 13:34:07 -0500

    Convo Checkin Creation

--- checkInScript.js
+++ checkInScript.js
@@ -1,16 +1,30 @@
 window.onload = () => {
     const queryString = window.location.search; 
     const urlParams = new URLSearchParams(queryString);
-    const person = urlParams.get('person')
     const personID = urlParams.get('personID')
-    let personName = document.querySelector(".personName");
-    personName.innerHTML = person
+
 }
 const checkIn = () => {
     const queryString = window.location.search; 
     const urlParams = new URLSearchParams(queryString);
-    const person = urlParams.get('person')
     const personID = urlParams.get('personID')
+
+    
+    console.log(getCookie("hasMarked"))
+    if(getCookie("hasMarked") == "true"){
+      let attendanceLogBanner = document.querySelector(".attendanceBlurb");
+      attendanceLogBanner.innerHTML = `You have already marked yourself as present.`
+      attendanceLogBanner.classList.add("wrong")
+      return
+    }
+
+    
+
+    let button = document.querySelector(".mainBtn")
+    button.disabled = true;
+    button.innerHTML = "Please Wait";
+    button.classList.add("buttonDisabled")
+
     function distance(lon1, lat1, lon2, lat2) {
         var R = 6371; 
         var dLat = (lat2-lat1).toRad();  
@@ -34,16 +48,63 @@ const checkIn = () => {
 
       window.navigator.geolocation.getCurrentPosition(function(pos) {
         console.log(pos); 
-        let distancetoRC = distance(pos.coords.longitude, pos.coords.latitude, -85.10847247405334, 41.11991550988574 )
+        //, 
+        let distancetoRC = distance(pos.coords.longitude, pos.coords.latitude, -85.21229404568588, 41.12256011135039 )
         console.log(
             distancetoRC
         ); 
+
         if(distancetoRC < 0.15){
             console.log("Person in Radius")
+              //call AirTable API
+              var Airtable = require('airtable');
+              var base = new Airtable({apiKey: urlParams.get("ak")}).base('appVKJR76lWZ1teWE');
+              base('Test Table for Class').update([{
+                  "id": personID,
+                  "fields": {
+                    "Status": "Present"
+                  }
+                }], (err, records) => {
+                if (err) {
+                  console.error(err);
+                  attendanceLogBanner.innerHTML = `We experienced an error attempting to mark you as present. Please report this to Ronan Armstrong, and include the following error: ${err}`
+                attendanceLogBanner.classList.add("wrong")
+                  return;
+                }
+                var date = new Date()
+              date.setDate(date.getDate() + 1);
+              let gtmsDate = date.toGMTString();
+              document.cookie = `hasMarked=true; expires=${gtmsDate}`;
+                  
+                records.forEach((record) => {
+
+                  attendanceLogBanner.innerHTML = `We successfully marked you as present.`
+            attendanceLogBanner.classList.add("right")
+              
+                });
+              });
         }
         if(distancetoRC >= 0.15){
-            console.log("Person NOT in Radius")
-            //call AirTable API
+            let attendanceLogBanner = document.querySelector(".attendanceBlurb");
+            let distanceCalc = Math.ceil(distancetoRC)
+            attendanceLogBanner.innerHTML = `We were unable to mark you as present because your distance was ${distanceCalc} kilometers away from the Music Center.`
+            attendanceLogBanner.classList.add("wrong")
+
+            }
+        
+      }, function(error) {
+        if (error.code == error.PERMISSION_DENIED) {
+          let attendanceLogBanner = document.querySelector(".attendanceBlurb");
+            
+            attendanceLogBanner.innerHTML = `Please allow location access to mark your attendance.`
+            attendanceLogBanner.classList.add("wrong")
         }
+          
       });
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
